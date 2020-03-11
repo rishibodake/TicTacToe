a0fdac485e038de5fc5fdb911211d4d7aa9bef2f
rishibodake <rishibodake@gmail.com>
2020-03-11 13:03:31 +0530

    Added:ComputerPlayFeature

--- tikTakToe.sh
+++ tikTakToe.sh
@@ -7,6 +7,7 @@ COLUMN=3
 player="P"
 turn=1
 winChecker=0
+#cNumber=0
 declare -A board
 function initializeBoard(){  #function to create board
 	for((r=0;r<$ROW;r++))
@@ -30,14 +31,17 @@ function displayBoard(){  #function to displayboard
 	echo "---------------"
 
 }
-function toss(){
-	if [[  $((RANDOM%2)) -eq 0 ]]
-	then
-		echo Player 1 Term
-	else
-		echo Player 2 Term
-	fi
-}
+#function toss(){
+#	if [[  $((RANDOM%2)) -eq 0 ]]
+#	then
+#		echo "Player's Term"
+#		playerTurn
+#	else
+#		echo "Computer's Term"
+#		computerTurn
+#
+#	fi
+#}
 function assignLetter(){
 	if [[ $turn%2 -eq 0 ]]
 	then
@@ -45,30 +49,64 @@ function assignLetter(){
 	else
 		player="O"
 	fi
+
 }
+function computerPlay(){
+	cNumber=$((RANDOM%8+1))
+	r=$(($cNumber/3))
+	c=$(($cNumber%3))
+	if [[ ${board[$r,$c]} == - ]]
+	then
+		putInCell $cNumber
+	elif [[ ${board[$r,$c]} == X ]]
+	then
+		computerPlay
+	elif [[ ${board[$r,$c]} == O ]]
+	then
+		computerPlay
+	fi
+}
+
+
+function turnDecider(){
+   while [[ $turn -le 9 && winChecker -eq 0 ]]
+   do
+      if [[ $turn%2 -eq 0 ]]
+      then
+			computerPlay
+         echo "Computers choice: $cNumber"
+#         putInCell $cNumber
+         ((turn++))
+		else
+         echo "Players turn"
+         read -p "Enter your choice: " ch
+         putInCell $ch
+         ((turn++))
+	   fi
+   done
+}
+
 function putInCell(){
-	while [[ $turn -le 9 && winChecker -eq 0 ]]
-	do
+		choice=$1
 		assignLetter
-		read -p "Enter your choice: " choice
 		case $choice in
-						1)
+						0)
 							board[0,0]=$player;;
-						2)
+						1)
                      board[0,1]=$player;;
-						3)
+						2)
                      board[0,2]=$player;;
-						4)
+						3)
                      board[1,0]=$player;;
-						5)
+						4)
                      board[1,1]=$player;;
-						6)
+						5)
                      board[1,2]=$player;;
-						7)
+						6)
                      board[2,0]=$player;;
-						8)
+						7)
                      board[2,1]=$player;;
-						9)
+						8)
                      board[2,2]=$player;;
 						*)
 							printf "Associative key not find "
@@ -76,9 +114,8 @@ function putInCell(){
 		esac
 	displayBoard
 	isWon
-	((turn++))
-	done
 }
+
 function isWon(){
 	if [[ ${board[0,0]} == "X" && ${board[0,1]} == "X" && ${board[0,2]} == "X" ]]
 	then
@@ -138,7 +175,7 @@ function isWon(){
    fi
 
 }
-toss
+#toss
 initializeBoard
 displayBoard
-putInCell
+turnDecider
